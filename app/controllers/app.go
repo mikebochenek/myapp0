13c1b2bf9fd386b6299b4906f4cd32d1b21d32e4
mikebochenek <mike@bochenek.ca>
2013-06-07 23:18:50 +0200

    trying to connect to backend, and showing bootstrap table

--- app/controllers/app.go
+++ app/controllers/app.go
@@ -2,6 +2,9 @@ package controllers
 
 import "github.com/robfig/revel"
 import "time"
+import "fmt"
+import "database/sql"
+import _ "github.com/go-sql-driver/mysql"
 
 type App struct {
 	*revel.Controller
@@ -11,6 +14,20 @@ func (c App) Index() revel.Result {
 	now := time.Now()
 	greeting := ("Today is " + now.Format("Monday"))
 	greeting += (" " + now.Format(time.StampMicro))
+
+	con, err := sql.Open("mysql", "test:@/test")
+	if err != nil {
+		fmt.Print(err)
+	}
+
+	/*
+		row := con.QueryRow("select createDate from Draft where id=?", 490)
+			cb := nil
+			err := row.Scan(&cb)
+			fmt.Print(cb)
+	*/
+
+	defer con.Close()
 
 	return c.Render(greeting)
 }
