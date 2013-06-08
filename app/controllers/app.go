7eae39169023867110a5ee769c8e37d261f6857f
mikebochenek <mike@bochenek.ca>
2013-06-08 23:46:21 +0200

    mysql queries working now

--- app/controllers/app.go
+++ app/controllers/app.go
@@ -1,10 +1,12 @@
 package controllers
 
-import "github.com/robfig/revel"
-import "time"
-import "fmt"
-import "database/sql"
-import _ "github.com/go-sql-driver/mysql"
+import (
+	"database/sql"
+	"fmt"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/robfig/revel"
+	"time"
+)
 
 type App struct {
 	*revel.Controller
@@ -17,17 +19,39 @@ func (c App) Index() revel.Result {
 
 	con, err := sql.Open("mysql", "test:@/test")
 	if err != nil {
-		fmt.Print(err)
+		fmt.Println(err)
 	}
 
+	fmt.Println(greeting)
+
 	/*
-		row := con.QueryRow("select createDate from Draft where id=?", 490)
-			cb := nil
-			err := row.Scan(&cb)
-			fmt.Print(cb)
+		row := con.QueryRow("select id from Draft where id=?", 490)
+		var cb int
+		err = row.Scan(&cb)
+		fmt.Printf("output was: %d\n", cb)
 	*/
 
+	rows, err := con.Query("select id, text from Draft")
+	var cb int
+	var text string
+	for rows.Next() {
+		err = rows.Scan(&cb, &text)
+		fmt.Printf("draft read: id=%d len=%d\n", cb, len(text))
+	}
+
+	var count int
+	rows, err = con.Query("select id, sentence from Recommendation")
+	for rows.Next() {
+		count++
+		err = rows.Scan(&cb, &text)
+		//fmt.Printf("recommendation read: id=%d len=%d\n", cb, len(text))
+	}
+	fmt.Printf("count was %d.\n", count)
+
 	defer con.Close()
+
+	expired := (time.Now().Nanosecond() - now.Nanosecond())
+	fmt.Printf("microseconds expired: %d (which is about %dms)\n", expired/1000, expired/1000000)
 
 	return c.Render(greeting)
 }
